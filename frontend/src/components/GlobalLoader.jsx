4feb60390f6db6e8403b1697809d6e0bf9156688
Serdar Altanay <altanays14@gmail.com>
2026-02-26 01:36:34 +0300

    Completely remove GlobalLoader from DOM per user request

--- frontend/src/components/GlobalLoader.jsx
+++ frontend/src/components/GlobalLoader.jsx
@@ -3,20 +3,5 @@ import { apiLoadingState } from "../api/axios";
 import "./GlobalLoader.scss";
 
 export default function GlobalLoader() {
-    const [isLoading, setIsLoading] = useState(apiLoadingState.activeRequests > 0);
-
-    useEffect(() => {
-        const unsubscribe = apiLoadingState.subscribe(setIsLoading);
-        return () => unsubscribe();
-    }, []);
-
-    if (!isLoading) return null;
-
-    return (
-        <div className="global-loader-overlay">
-            <div className="global-loader-bar">
-                <div className="global-loader-progress"></div>
-            </div>
-        </div>
-    );
+    return null; // Tamamen iptal edildi (Kullanıcı isteği)
 }
